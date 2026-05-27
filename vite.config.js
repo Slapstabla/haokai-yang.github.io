8a803e3e93664c848d3f93dcd1b415b743661521
Daniel AGI <haokai.yang@kiski.org>
2026-05-26 22:54:05 -0400

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/haokai-yang.github.io/',  
+  base: './',  
   server: {
     host: '127.0.0.1',
   },
